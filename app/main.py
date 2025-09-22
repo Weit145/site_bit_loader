e608708f3491433520a49a96bf6655b74e69241e
Weit145 <weit.dima@yandex.ru>
2025-09-22 07:56:41 +0300

    Tken begin

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 import uvicorn
 
 from users.views import router as users_router
 
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 app=FastAPI()
 app.include_router(users_router,tags=["Users"])
 
