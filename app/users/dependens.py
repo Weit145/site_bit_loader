60e92329137597c21a5a6cade75e0472c4f9f607
Weit145 <weit.dima@yandex.ru>
2025-09-22 18:04:07 +0300

    Login_for_access_token

--- app/users/dependens.py
+++ app/users/dependens.py
@@ -6,13 +6,30 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper
 from . import crud
-from .schemas import User,   Create_User
-
-async def user_by_id(user_id:Annotated[int,Path],session:AsyncSession = Depends(db_helper.session_dependency)):
-    product=await crud.get_user(session=session,user_id=user_id)
-    if product is not None:
-        return product
+from .schemas import User
+async def user_by_id(
+    user_id: Annotated[int, Path],
+    session: AsyncSession = Depends(db_helper.session_dependency)
+) -> User:
+    user = await session.get(User, user_id)
+    if user is not None:
+        return user
+    
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Product  {user_id} not found"
+        detail=f"User {user_id} not found"
+    )
+
+async def user_by_username(
+    username: Annotated[str, Path],
+    session: AsyncSession = Depends(db_helper.session_dependency)
+):
+    from . import crud 
+    user = await crud.get_user(session, username)
+    if user is not None:
+        return user
+    
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"User {username} not found"
     )
--- app/users/schemas.py
+++ app/users/schemas.py
@@ -1,12 +1,12 @@
 from annotated_types import MaxLen, MinLen
 from pydantic import BaseModel, ConfigDict
-
 from typing import Annotated
 
 class UserBase(BaseModel):
-    username:Annotated[str,MinLen(4),MaxLen(32)]
+    username: Annotated[str, MinLen(4), MaxLen(32)]
+
+class AvtorUser(UserBase):
     disabled: bool | None = None
-    pass
 
 class Token(BaseModel):
     access_token: str
@@ -14,12 +14,14 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: str | None = None
+
 class Create_User(UserBase):
-    password:Annotated[str,MaxLen(6),MaxLen(32)]
-    pass
-class Enter_User(Create_User):
-    pod_password:Annotated[str,MaxLen(6),MaxLen(32)]
-    pass
+    password: Annotated[str, MinLen(6), MaxLen(32)]  # Исправлено имя поля
+
+class Enter_User(UserBase):
+    password: Annotated[str, MinLen(6), MaxLen(32)]
+    pod_password: Annotated[str, MinLen(6), MaxLen(32)]
+
 class User(UserBase):
-    model_config=ConfigDict(from_attributes=True)
-    id:int
+    model_config = ConfigDict(from_attributes=True)
+    id: int
