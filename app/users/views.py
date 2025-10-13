0d0a969b8042aa79aff5f58d2d02e964b76c0326
Weit145 <weit.dima@yandex.ru>
2025-10-13 16:50:11 +0300

    dependes import

--- app/users/views.py
+++ app/users/views.py
@@ -1,19 +1,32 @@
 from typing import Annotated
 
-from core.models.db_hellper import db_helper
 from fastapi import APIRouter, Depends, status
-from profiles.crud import Clear_Upload_dir, Create_Profile
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from . import crud, dependens, token
-from .schemas import Token, UserCreate, UserGet, UserLogin, UserResponse
+from core.models.db_hellper import db_helper
+from profiles.crud import Clear_Upload_dir, Create_Profile
+
+from .dependens import (
+    UserForm_TO_UserCreate,
+    Get_Current_User,
+    UserForm_TO_UserLogin,
+    User_By_Id_Path,
+)
+from . import crud, token
+from .schemas import (
+    Token,
+    UserCreate,
+    UserGet,
+    UserLogin,
+    UserResponse,
+)
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("/", response_model=Token)
 async def Create_User_EndPoint(
-    user_create: Annotated[UserCreate, Depends(dependens.UserForm_TO_UserCreate)],
+    user_create: Annotated[UserCreate, Depends(UserForm_TO_UserCreate)],
     session: Annotated[AsyncSession, Depends(db_helper.session_dependency)],
 ) -> Token:
     user = await crud.Create_User(session=session, user_create=user_create)
@@ -27,7 +40,7 @@ async def Create_User_EndPoint(
 
 @router.delete("/me/", status_code=status.HTTP_204_NO_CONTENT)
 async def Delete_Me_User_EndPoint(
-    current_user: Annotated[UserResponse, Depends(dependens.Get_Current_User)],
+    current_user: Annotated[UserResponse, Depends(Get_Current_User)],
     session: Annotated[AsyncSession, Depends(db_helper.session_dependency)],
 ) -> None:
     await crud.Delete_User(session=session, user_id=current_user.id)
@@ -43,7 +56,7 @@ async def Delete_All_Users_EndPoint(
 
 @router.post("/token/", response_model=Token)
 async def Login_For_Access_Token_EndPoint(
-    form_data: Annotated[UserLogin, Depends(dependens.UserForm_TO_UserLogin)],
+    form_data: Annotated[UserLogin, Depends(UserForm_TO_UserLogin)],
     session: Annotated[AsyncSession, Depends(db_helper.session_dependency)],
 ) -> Token:
     user = await crud.Authenticate_User(session=session, user=form_data)
@@ -53,13 +66,13 @@ async def Login_For_Access_Token_EndPoint(
 
 @router.get("/me/", response_model=UserResponse)
 async def Read_Me_User_EndPoint(
-    current_user: Annotated[UserResponse, Depends(dependens.Get_Current_User)],
+    current_user: Annotated[UserResponse, Depends(Get_Current_User)],
 ) -> UserResponse:
     return current_user
 
 
 @router.get("/{user_id}/", response_model=UserGet)
 async def Get_User_By_Id_EndPoint(
-    user: Annotated[UserResponse, Depends(dependens.User_By_Id_Path)],
+    user: Annotated[UserResponse, Depends(User_By_Id_Path)],
 ) -> UserResponse:
     return user
