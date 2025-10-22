8e3eb0dbf3f622ceeead24bf0647c0bec04bf7cf
Weit145 <weit.dima@yandex.ru>
2025-10-22 23:48:07 +0300

    send email

---
+++ app/tasks/email/email.py
@@ -0,0 +1,36 @@
+import aiosmtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+
+
+async def send_email(
+    recipient: str,
+    subject: str,
+    plain_content: str,
+    html_content: str = "",
+):
+    sender_email = os.getenv("YANDEX_USER")
+    app_password = os.getenv("YANDEX_APP_PASSWORD")
+
+    message = MIMEMultipart("alternative")
+    message["From"] = sender_email
+    message["To"] = recipient
+    message["Subject"] = subject
+
+    plain_text_message = MIMEText(plain_content, "plain", "utf-8")
+    message.attach(plain_text_message)
+
+    if html_content:
+        html_message = MIMEText(html_content, "html", "utf-8")
+        message.attach(html_message)
+
+
+    await aiosmtplib.send(
+        message,
+        hostname="smtp.yandex.ru",
+        port=465,
+        username=sender_email,
+        password=app_password,
+        use_tls=True,
+    )
--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -1,6 +1,23 @@
 from app.tasks.celery import app
+import asyncio
 
-@app.task(name="app.tasks.send_email.send_message")
+from app.tasks.email.email import send_email
+
+# @app.task(name="app.tasks.send_email.send_message")
+# def send_message():
+#     print("✅ send_message выполнена")
+#     return "Hello World"
+
+
+
+@app.task(name="send_email")
 def send_message():
-    print("✅ send_message выполнена")
-    return "Hello World"
+    asyncio.run(
+        send_email(
+            recipient="kocnevs.jk@yandex.ru",
+            subject="Рыжий лох",
+            plain_content="xd",
+            html_content="<h3>Вопросы?</h3><p>А<b>А</b>А<i>А</i>?</p>",
+        )
+    )
+    return "Письмо отправлено успешно"
