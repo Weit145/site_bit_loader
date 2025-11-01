7588860768be596c00bcbd27843197a771326f32
Weit145 <weit.dima@yandex.ru>
2025-11-01 14:19:13 +0300

    test

---
+++ tests/test_api.py
@@ -0,0 +1,38 @@
+import pytest
+
+from httpx import AsyncClient,ASGITransport
+
+from app.main import app
+
+from unittest.mock import patch
+
+@pytest.mark.asyncio
+async def test_get_all_posts_end_point():
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://test",
+    ) as ac:
+        response = await ac.get("/post/")
+        assert response.status_code == 200
+
+
+@patch("app.users.crud.send_message.delay")
+@pytest.mark.asyncio
+async def test_create_user_end_point(mock_delay):
+    mock_delay.return_value = None
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response = await ac.post("/user/registration/", json={
+            "username": "Weit",
+            "password": "123456",
+            "email": "kloader145@gmail.com"
+        })
+    assert response.status_code == 200
+
+@pytest.mark.asyncio
+async def test_delete_all_users_end_point():
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://test",
+    ) as ac:
+        response = await ac.delete("/user/admin/")
+        assert response.status_code == 204
